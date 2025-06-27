1f0b556c9636b84ca5afa9564be6fc5bdc1e1521
Benjamin Correa <iambenjamin.developer@gmail.com>
2025-06-26 22:56:41 -0300

    ---

--- src/Inventory/Inventory.API/Controllers/ValuesController.cs
+++ src/Inventory/Inventory.API/Controllers/ValuesController.cs
@@ -1,63 +1,43 @@
-﻿//using Inventory.API.Producers;
-//using MassTransit;
-//using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
-//namespace Inventory.API.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class ValuesController : ControllerBase
-//    {
-//        private readonly IPublishEndpoint _publishEndpoint;
-//        private readonly QueueProducerService _queueProducerService;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-//        public ValuesController(IPublishEndpoint publishEndpoint, QueueProducerService queueProducerService)
-//        {
-//            _publishEndpoint = publishEndpoint;
-//            _queueProducerService = queueProducerService;
-//        }
+namespace Inventory.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
 
-//        // POST /api/products
-//        [HttpGet("SendEvent")]
-//        public async Task<ActionResult> SendEvent()
-//        {
-//            await _queueProducerService.SendSubscribeProductEvent();
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
 
-//            return Ok();
-//        }
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
 
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
 
-
-//        // GET: api/<ValuesController>
-//        [HttpGet]
-//        public IEnumerable<string> Get()
-//        {
-//            return new string[] { "value1", "value2" };
-//        }
-
-//        // GET api/<ValuesController>/5
-//        [HttpGet("{id}")]
-//        public string Get(int id)
-//        {
-//            return "value";
-//        }
-
-//        // POST api/<ValuesController>
-//        [HttpPost]
-//        public void Post([FromBody] string value)
-//        {
-//        }
-
-//        // PUT api/<ValuesController>/5
-//        [HttpPut("{id}")]
-//        public void Put(int id, [FromBody] string value)
-//        {
-//        }
-
-//        // DELETE api/<ValuesController>/5
-//        [HttpDelete("{id}")]
-//        public void Delete(int id)
-//        {
-//        }
-//    }
-//}
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
