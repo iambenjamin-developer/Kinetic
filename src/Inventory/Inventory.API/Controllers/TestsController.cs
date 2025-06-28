3498fd45eb5b33ad536bfb839fa8f970a289988d
Benjamin Correa <iambenjamin.developer@gmail.com>
2025-06-28 12:51:04 -0300

    test controller

---
+++ src/Inventory/Inventory.API/Controllers/TestsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestsController : ControllerBase
+    {
+
+        [HttpGet("DateTime")]
+        public IActionResult Get()
+        {
+            var result = new
+            {
+                Date = DateTime.UtcNow.ToLongDateString(),
+                Time = DateTime.UtcNow.ToLongTimeString()
+            };
+
+            return Ok(result);
+        }
+
+    }
+}
